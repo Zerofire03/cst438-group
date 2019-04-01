a490ac2db43cf5cd63ef048f65eeda86ac667a5d
Christopher Holmes <cholmes@csumb.edu>
2019-04-01 23:21:34 +0000

    Create sessions controller and testing new author

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'sessions/directory'
+
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
