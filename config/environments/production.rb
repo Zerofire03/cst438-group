aab6ec0aaf22c045ce311cba671f5614bb40eccd
Christopher Holmes <zerofire.3@gmail.com>
2019-04-09 20:05:27 +0000

    removed config.active_record.dump_schema_after_migration = false as no longer supported in rails 5.2.x

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,7 +83,7 @@ Rails.application.configure do
   end
 
   # Do not dump schema after migrations.
-  config.active_record.dump_schema_after_migration = false
+  #config.active_record.dump_schema_after_migration = false
   
   config.action_mailer.delivery_method = :smtp
   # SMTP settings for gmail
