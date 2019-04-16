e5b282800ddefe47166ab127dd83c306ec992b8a
Christopher Holmes <zerofire.3@gmail.com>
2019-04-16 21:04:01 +0000

    Working on allowing profile to be uploaded with no resume

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -69,7 +69,10 @@ class ProfilesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def profile_params
-      params.require(:profile).permit(:name, :address, :phone_no, :interest, :skills, :resume)
-      #params.require(:profile).permit(:name, :address, :phone_no, :interest, :skills)
+      if params[:resume]
+        params.require(:profile).permit(:name, :address, :phone_no, :interest, :skills, :resume)
+      else
+        params.require(:profile).permit(:name, :address, :phone_no, :interest, :skills)
+      end
     end
 end
